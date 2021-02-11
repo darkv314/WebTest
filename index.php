4aa2ce42f0384f621b722f4baba00102951a0034
Ricardo Tejerina <ricardo.tejerina314@gmail.com>
2021-02-11 16:21:13 -0400

    Primer commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php 
+    session_start();
+
+    if(isset($_SESSION['user_id']))
+    {
+        unset($_SESSION['user_id']);
+    }
+
+    include "db.php";
+
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        //read from db
+        $user_id = $_POST['user_id'];
+        $ci = $_POST['ci'];
+        if(!empty($user_id) && !empty($ci))
+        {
+            $query = "select * from students where user_id = '$user_id' limit 1";
+            $result = mysqli_query($con, $query);
+            if($result && mysqli_num_rows($result) > 0)
+            {
+                $user_data = mysqli_fetch_assoc($result);
+                if($user_data['CI'] === $ci && $user_data['user_id'] === $user_id)
+                {
+                    $_SESSION['user_id'] = $user_data['user_id'];
+                    header("Location: vote.php");
+                    die();
+                }
+            }
+            echo "Ingresa información valida!";
+        }
+        else
+        {
+            echo "Ingresa información valida!";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width,
+        initial-scale=1.0">
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <title>Inicio de sesión</title>
+    </head>
+    <body>
+        <div class="header">
+            <h1>Elección de capitanes 2021<br>
+            SEUPB</h1>
+        </div>
+        <div class="middle">
+            <div class="col1">
+                <h3>Rumbo al UPB Match 2021</h3>
+                <p>El primer paso para apoyar a la camiseta y darlo todo por tu carrera</p>
+            </div>
+            <div class="main">
+                <!-- CSS para las flechas  -->
+                <form method="post">
+                    Código de estudiante <input type="number" name="user_id" placeholder="Ej: 12345">
+                    Carnet de identidad <input type="number" name="ci" placeholder="11111111">
+                    <input type="submit" name="submit" value="Enviar">
+                </form> 
+            </div>
+            <div class="col2">
+                <h3>¿Estas list@?</h2>
+            </div>
+        </div>
+        <div class="footer">
+            Secretaría de Tecnología e Innovación - ISC 2021<br>
+            SEUPB 2021
+        </div>
+    </body>
+</html>
---
+++ vote.php
@@ -0,0 +1,61 @@
+<?php
+    session_start();
+
+    include "db.php";
+    include "functions.php";
+
+    $user_data = check_login($con);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+        <title>Vota por tu capitán</title>
+    </head>
+    <body>
+        <div class="header">
+            <h1>Elección de capitanes 2021<br>
+            SEUPB</h1>
+        </div>
+        <div class="container">
+            <div class="card">
+                <div class="imgBx">
+                    <img src="images/image1.jpg">
+                </div>
+                <div class="content">
+                    <h2>Card one</h2>
+                    <p>Insert some text lol</p>
+                    <a href="vote.php">Votar</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="imgBx">
+                    <img src="images/image2.jpg">
+                </div>
+                <div class="content">
+                    <h2>Card two</h2>
+                    <p>Insert some text lol</p>
+                    <a href="vote.php">Votar</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="imgBx">
+                    <img src="images/image3.jpg">
+                </div>
+                <div class="content">
+                    <h2>Card three</h2>
+                    <p>Al obstinado escalador le tomó años de intentos y fracasos llegar a la cima para descubrir, tristemente, que allí tampoco está la felicidad.</p>
+                    <a href="vote.php">Votar</a>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            Secretaría de Tecnología e Innovación - ISC 2021<br>
+            SEUPB 2021
+        </div>
+    </body>
+</html>
+
